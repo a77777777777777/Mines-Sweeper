ce94d11b8f45786e53372fdc3e29da7231b76f1f
AadhiNarayanan7 <aadhinarayanan7@yahoo.com>
2025-01-08 20:41:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ function createGrid(h,w){
         });
         document.getElementById(a+","+b).addEventListener("touchend",function(e){
             if(isStarted){
-                setTimeout(setflag(this.id),310);
+                setTimeout(setflag(this.id),440);
             }
         });
     }
